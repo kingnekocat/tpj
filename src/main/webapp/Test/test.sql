d454d971ab88d6e39a2c7f05b942e14b5d67afe9
jys <rnfma6469@gmail.com>
2022-06-17 09:44:23 +0900

    db 수정

--- src/main/webapp/Test/test.sql
+++ src/main/webapp/Test/test.sql
@@ -1,12 +1,13 @@
 create table friendlist (
-	f_num number(3) not null,
+	f_num number(3) primary key not null,
 	f_myid varchar2 (30 char) not null,
-	f_yourid varchar2 (30 char) primary key not null
+	f_yourid varchar2 (30 char) not null
 )
 				
 create sequence friendlist_seq;
 				
 select * from friendlist;
+drop table friendlist;
 
 select * from friendlist where f_myid=4;
 
