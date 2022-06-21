f3e7081df971a43d48cdb32a8d459fcc4c8811e6
shc <luckzoayon@gmail.com>
2022-06-21 16:32:50 +0900

    머지 후 커밋

--- src/main/java/com/team/main/DBManager.java
+++ src/main/java/com/team/main/DBManager.java
@@ -12,8 +12,7 @@ public static Connection connect() throws SQLException {
 		
 
 	String url = "jdbc:oracle:thin:@localhost:1521:xe";
-
-		return DriverManager.getConnection(url, "c##ej1234", "ej1234");
+	return DriverManager.getConnection(url,"shc","shc");
 
 	}
 	
