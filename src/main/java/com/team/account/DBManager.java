8a34ebfb6d6a2030ef99208b71a91eb313abfba2
shc <luckzoayon@gmail.com>
2022-06-24 12:15:48 +0900

    대괄호 커밋 수정

--- src/main/java/com/team/account/DBManager.java
+++ src/main/java/com/team/account/DBManager.java
@@ -13,11 +13,10 @@ public static Connection connect() throws SQLException {
 		
 
 	String url = "jdbc:oracle:thin:@localhost:1521:xe";
-
-	return DriverManager.getConnection(url,"c##ej1234","ej1234");
-
+	return DriverManager.getConnection(url,"shc","shc");
 
 
+}
 	
 	
 	public static void close(Connection con, PreparedStatement pstmt, ResultSet rs) {
