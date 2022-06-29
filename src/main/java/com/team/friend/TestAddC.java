a1ce2de36c7ac248dad1eb4a0de9c0da4e4dd50a
jys <rnfma6469@gmail.com>
2022-06-29 14:45:23 +0900

    friendlist add commit

--- src/main/java/com/team/friend/TestAddC.java
+++ src/main/java/com/team/friend/TestAddC.java
@@ -7,16 +7,24 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.team.account.Account;
 import com.team.account.AccountDAO;
+import com.team.menu1.Menu1DAO;
 
 @WebServlet("/TestAddC")
 public class TestAddC extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		Account a = (Account)request.getSession().getAttribute("accountInfo");
 		AccountDAO.loginCheck(request);
-		frinedlistDAO.addFriend(request);
-
-		frinedlistDAO.ViewFriendList(request);
-		request.setAttribute("contentPage", "Test/test_list.jsp");
+		if (a!=null) {
+			frinedlistDAO.addFriend(request);
+			frinedlistDAO.ViewFriendList(request);
+			request.setAttribute("contentPage", "Test/test_list.jsp");
+		} else {
+			request.setAttribute("contentPage", "home.jsp");
+			request.getRequestDispatcher("index.jsp").forward(request, response);
+		}
 
 		
 
